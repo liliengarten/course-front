4b3936d02fd54644fa11aee72d060babb77478ef
liliengarten <liliengarten333@gmail.com>
2025-12-12 19:21:34 +0700

    klyanus gotovo

--- cypress/e2e/catalog_and_cart.cy.js
+++ cypress/e2e/catalog_and_cart.cy.js
@@ -36,4 +36,16 @@ describe('Каталог и корзина', () => {
         cy.contains('Очистить корзину').click();
         cy.get('.cartItem').should('not.exist');
     })
+
+    it('Оформление заказа', () => {
+        cy.signin()
+        cy.url().should('include', '/catalog');
+        cy.contains('Корзина').click()
+        cy.get("div[name=catalogCard]").eq(0).contains('+').click()
+        cy.get("div[name=catalogCard]").eq(1).contains('+').click()
+        cy.contains('Оформить заказ').click();
+
+        cy.contains('Заказы').click();
+        cy.get('.orderWrapper').should('exist');
+    })
 })
